b3bc02ed11a151f0acb64f912d5557ec7afdbeae
raghu0056 <raghumani0056@gmail.com>
2023-03-11 17:07:47 +0530

    Print the array elements

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int [] arr = new int [] {1, 2, 3, 4, 5};
+        System.out.println("Elements of given array: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
