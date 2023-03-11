6f390278a15b8ceb10c685879c580519d639896c
raghu0056 <raghumani0056@gmail.com>
2023-03-11 17:27:58 +0530

    Print the second largest element in array

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String args[]){
+        int temp, size;
+        int array[] = {1,2,3,4,5,6};
 
-        int [] arr = new int [] {1, 2, 3, 2,};
-        System.out.println("Duplicate elements in given array: ");
-        for(int i = 0; i < arr.length; i++)
-        {
-            for(int j = i + 1; j < arr.length; j++)
-            {
-                if(arr[i] == arr[j])
-                    System.out.println(arr[j]);
+        for(int i = 0; i<array.length; i++ ){
+            for(int j = i+1; j< array.length; j++){
+
+                if(array[i]>array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
             }
         }
+        System.out.println("Second largest number is:: "+array[array.length-2]);
     }
 }
 
