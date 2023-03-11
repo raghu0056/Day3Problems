96434d400041a9c6bb0f4f35b922712b669ad0cb
raghu0056 <raghumani0056@gmail.com>
2023-03-11 17:12:59 +0530

    Print the smallest number in array

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main{
     public static void main(String[] args) {
-        int [] arr = new int [] {25, 11, 7, 75, 56};
+        int [] arr = new int [] {2,100, 7, 75, 56};
 
         int max = arr[0];
 
         for (int i = 0; i < arr.length; i++) {
 
-            if(arr[i] > max)
+            if(arr[i] < max)
                 max = arr[i];
         }
         System.out.println("Largest element present in given array: " + max);
     }
 }
+
